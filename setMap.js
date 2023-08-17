f984285f3bf2979264e7d57d5f8a0db40feb1524
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-08-17 03:32:01 -0300

    add set map data structures file

---
+++ setMap.js
@@ -0,0 +1,33 @@
+const set = new Set()
+
+set.add(2)
+set.add(3)
+set.add(2)
+set.add(1)
+
+// set.clear()
+// set.delete(3)
+// console.log(set)
+
+const map = new Map()
+
+map.set("rj", "rio de janeiro")
+map.set("sp", "sao paulo")
+map.set("pr", "parana")
+map.set("sc", "santa catarina")
+map.set("rs", "rio grande do sul")
+
+// map.forEach((value, key) => {
+//   console.log(value, "-", key)
+// })
+
+console.log(map.entries())
+
+// console.log(map.delete("rj"))
+// console.log(map.get("rs"))
+// console.log(map.size())
+// console.log(map.has())
+// console.log(map.entries())
+// map.clear()
+
+// console.log(map)
